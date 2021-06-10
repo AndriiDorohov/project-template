229cd01d9daffb48e87dc3db4e3f2b7554d88a7c
Andrii Dorohov <wedpositive@gmail.com>
2021-06-10 11:19:21 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './sass/main.scss';
+import fetchFiltm from './js/fetchFilm-test.js';
+import './js/pagination.js';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import './js/studentsGroup';
+
